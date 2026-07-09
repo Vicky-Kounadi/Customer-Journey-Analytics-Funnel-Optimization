3236614bbe1b1f46ca6aba2afd2e1ff5ca2f990e
Vasiliki Kounadi <116192912+Vicky-Kounadi@users.noreply.github.com>
2026-07-09 09:55:25 +0300

    Check if my tables aalready exist

---
+++ sql/02_create_tables.sql
@@ -0,0 +1,11 @@
+-- CUSTOMER JOURNEY ANALYTICS + FUNNEL OPTIMIZING
+-- Create the projects tables setup in a star scheme fashion
+
+-- Just checks to scrap old db
+DROP TABLE IF EXISTS fact_events;
+DROP TABLE IF EXISTS dim_users;
+DROP TABLE IF EXISTS dim_calendar;
+DROP TABLE IF EXISTS dim_events;
+DROP TABLE IF EXISTS dim_product_categories;
+DROP TABLE IF EXISTS dim_channels;
+DROP TABLE IF EXISTS dim_devices;

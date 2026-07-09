d259f8d1934d48435ccca02b834d2a393fe36621
Vasiliki Kounadi <116192912+Vicky-Kounadi@users.noreply.github.com>
2026-07-09 11:58:00 +0300

    Duplicates check

--- sql/04_data_check.sql
+++ sql/04_data_check.sql
@@ -3,4 +3,11 @@
 
 -- Records num
 SELECT COUNT(*) FROM raw_events;
+-- 21409 records
 
+-- Dupliactes
+SELECT *, COUNT(*) AS times
+FROM raw_events
+GROUP BY user_id, session_id, event_time, event, device, region, channel, product_category, revenue, bonus_flag
+HAVING COUNT(*) > 1;
+-- No duplicates

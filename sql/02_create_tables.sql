e84671eb7055264abdc1cda7d863e2cb704036e0
Vasiliki Kounadi <116192912+Vicky-Kounadi@users.noreply.github.com>
2026-07-09 10:01:40 +0300

    Create device table

--- sql/02_create_tables.sql
+++ sql/02_create_tables.sql
@@ -9,3 +9,11 @@ DROP TABLE IF EXISTS dim_events;
 DROP TABLE IF EXISTS dim_product_categories;
 DROP TABLE IF EXISTS dim_channels;
 DROP TABLE IF EXISTS dim_devices;
+
+-- Dimension table for DEVICE types used for customer sessions
+CREATE TABLE dim_devices (
+	device_id INT PRIMARY KEY AUTO_INCREMENT,
+    device_name VARCHAR(50) UNIQUE NOT NULL
+);
+
+SELECT * FROM dim_devices;

9414292a1e0fbcda212a914fabe2c8a511c24398
Vasiliki Kounadi <116192912+Vicky-Kounadi@users.noreply.github.com>
2026-07-09 11:45:55 +0300

    Import raw data from csv

--- sql/03_import_raw_data.sql
+++ sql/03_import_raw_data.sql
@@ -1,6 +1,10 @@
 -- CUSTOMER JOURNEY ANALYTICS + FUNNEL OPTIMIZING
 -- Import CSV and make a table to store the raw data
 
+SET GLOBAL local_infile = 1;
+SHOW VARIABLES LIKE 'local_infile';
+SHOW VARIABLES LIKE 'secure_file_priv';
+
 CREATE TABLE raw_events (
 	user_id VARCHAR(20),
     session_id VARCHAR(50), 
@@ -11,8 +15,17 @@ CREATE TABLE raw_events (
 	channel VARCHAR(50),
 	product_category VARCHAR(50),
     revenue DECIMAL(15,2) DEFAULT 0.00,
-	bonus_flag BOOLEAN
+	bonus_flag VARCHAR(5)
 );
+
+SELECT * FROM raw_events;
+SELECT COUNT(*) FROM raw_events;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Funnel_Analysis_Data.csv'
+INTO TABLE raw_events
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
 
 SELECT * FROM raw_events;
 SELECT COUNT(*) FROM raw_events;

71e3460cebbdd546e8795f78d5a0ea13e27850bd
Vasiliki Kounadi <116192912+Vicky-Kounadi@users.noreply.github.com>
2026-07-11 10:05:10 +0300

    Missing values-Negative money check

--- sql/04_data_check.sql
+++ sql/04_data_check.sql
@@ -11,3 +11,24 @@ FROM raw_events
 GROUP BY user_id, session_id, event_time, event, device, region, channel, product_category, revenue, bonus_flag
 HAVING COUNT(*) > 1;
 -- No duplicates
+
+-- Missing values anywhere
+SELECT *
+FROM raw_events
+WHERE user_id IS NULL OR 
+	session_id IS NULL OR 
+	event_time IS NULL OR
+	event IS NULL OR
+	device IS NULL OR
+	region IS NULL OR
+	channel IS NULL OR
+	product_category IS NULL OR 
+	revenue IS NULL OR 
+	bonus_flag IS NULL;
+-- None
+
+-- Negative money
+SELECT *
+FROM raw_events
+WHERE revenue < 0;
+-- No

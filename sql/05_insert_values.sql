1672829294452311a3fc752077e4ab9f0350b4ce
Vasiliki Kounadi <116192912+Vicky-Kounadi@users.noreply.github.com>
2026-07-14 23:21:30 +0300

    Insert channels, product data

--- sql/05_insert_values.sql
+++ sql/05_insert_values.sql
@@ -8,3 +8,19 @@ FROM raw_events;
 
 SELECT * FROM dim_devices;
 SELECT COUNT(*) FROM dim_devices;
+
+-- CHANNELS
+INSERT INTO dim_channels(channel_name)
+SELECT DISTINCT channel
+FROM raw_events;
+
+SELECT * FROM dim_channels;
+SELECT COUNT(*) FROM dim_channels;
+
+-- PRODUCT CATEGORIES
+INSERT INTO dim_product_categories(product_category_name)
+SELECT DISTINCT product_category
+FROM raw_events;
+
+SELECT * FROM dim_product_categories;
+SELECT COUNT(*) FROM dim_product_categories;

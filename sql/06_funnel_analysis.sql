850286183bc31171e8e0d6acbac898316e94471d
Vasiliki Kounadi <116192912+Vicky-Kounadi@users.noreply.github.com>
2026-09-08 21:42:51 +0300

    Users per stage percentage, conversion

--- sql/06_funnel_analysis.sql
+++ sql/06_funnel_analysis.sql
@@ -6,3 +6,18 @@ SELECT event_name, COUNT(DISTINCT user_id) AS users
 FROM fact_events fe
 JOIN dim_events de ON fe.event_id=de.event_id
 GROUP BY fe.event_id;
+
+-- What is the conversion rate between each stage?
+WITH 
+funnel_stage AS(
+	SELECT fe.event_id, event_name, COUNT(DISTINCT user_id) AS users
+	FROM fact_events fe
+	JOIN dim_events de ON fe.event_id=de.event_id
+	GROUP BY fe.event_id
+)
+SELECT event_id, event_name, users, -- LAG(users) OVER (ORDER BY event_id) AS prev,
+	CASE WHEN event_id=1
+		THEN 100
+        ELSE ROUND(users / (LAG(users) OVER (ORDER BY event_id)) * 100, 2)
+	END AS conversion_rate
+FROM funnel_stage;

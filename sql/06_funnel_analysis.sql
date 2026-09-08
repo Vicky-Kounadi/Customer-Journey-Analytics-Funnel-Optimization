d0413c777a1372b5b4a7fa21c4de3724c98f6796
Vasiliki Kounadi <116192912+Vicky-Kounadi@users.noreply.github.com>
2026-09-08 22:34:48 +0300

    How much does each stage add to the total rev

--- sql/06_funnel_analysis.sql
+++ sql/06_funnel_analysis.sql
@@ -83,3 +83,19 @@ SELECT CASE
 	ROUND( AVG(time_diff), 2) AS diff_minutes
 FROM time_difference
 GROUP BY transition;
+
+-- Stage help to total revenue
+WITH 
+funnel_stage AS(
+	SELECT fe.event_id, event_name, COUNT(DISTINCT user_id) AS users, SUM(revenue) as revenue
+	FROM fact_events fe
+	JOIN dim_events de ON fe.event_id=de.event_id
+	GROUP BY fe.event_id
+)
+SELECT event_id, event_name, users, -- LAG(users) OVER (ORDER BY event_id) AS prev,
+	CASE WHEN event_id=1
+		THEN 100
+        ELSE ROUND(users / (LAG(users) OVER (ORDER BY event_id)) * 100, 2)
+	END AS conversion_rate,
+    revenue
+FROM funnel_stage fs;

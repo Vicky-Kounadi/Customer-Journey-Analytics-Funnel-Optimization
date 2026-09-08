d7a50e55ce8b90113abea6c9c46d72ac7915e1e0
Vasiliki Kounadi <116192912+Vicky-Kounadi@users.noreply.github.com>
2026-09-08 20:51:18 +0300

    Users per stage

--- sql/06_funnel_analysis.sql
+++ sql/06_funnel_analysis.sql
@@ -1,2 +1,8 @@
 -- CUSTOMER JOURNEY ANALYTICS + FUNNEL OPTIMIZING
 -- Perform EDA and Funnel analysis on my db
+
+-- How many users reached each stage?
+SELECT event_name, COUNT(DISTINCT user_id) AS users
+FROM fact_events fe
+JOIN dim_events de ON fe.event_id=de.event_id
+GROUP BY fe.event_id;
